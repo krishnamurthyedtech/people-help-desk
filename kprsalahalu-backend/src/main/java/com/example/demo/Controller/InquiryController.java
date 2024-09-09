b1dfcef9965ef8de5d1a5a53038b19d14a2eae31
ganeshsubbara <ganeshsanapa@gmail.com>
2024-09-09 18:29:38 +0530

    Configuration done

--- kprsalahalu-backend/src/main/java/com/example/demo/Controller/InquiryController.java
+++ kprsalahalu-backend/src/main/java/com/example/demo/Controller/InquiryController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,6 +20,7 @@ import com.example.demo.Service.InquiryService;
 
 @RestController
 @RequestMapping("/inquiry")
+
 public class InquiryController {
 
 	@Autowired
