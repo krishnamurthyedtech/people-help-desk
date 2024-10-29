6cce8f36940edeea966aabf625f8dea7190221a4
siva4616 <bunnysiva6281@gmail.com>
2024-10-29 10:18:14 +0530

    comments

--- kprsalahalu-backend/src/main/java/com/example/demo/DTO/InquiryDetailsDTO.java
+++ kprsalahalu-backend/src/main/java/com/example/demo/DTO/InquiryDetailsDTO.java
@@ -1,4 +1,23 @@
 package com.example.demo.DTO;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class InquiryDetailsDTO {
+    private int id;
+    private String name;
+    private String subject;
+    private String inquiryType;
+    private String description;
+    private String phoneNo;
+    private LocalDateTime creationTime;
+    private List<CommentDetailsDTO> comments;  // List of comment DTOs
 }
+
